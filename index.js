762ae6f5d34de9f4c9a02a0c5e9cd79af1fb4b1d
Fernando Soares <fernando@mbp-frrrnd.local>
2018-06-20 20:57:57 -0300

    subindo

---
+++ index.js
@@ -0,0 +1,37 @@
+let els = document.querySelectorAll('[data-appear]')
+
+const isVisible = (elem) => {
+    let coords = elem.getBoundingClientRect()
+    let windowHeight = document.documentElement.clientHeight
+    let topVisible = coords.top + elem.clientHeight > 0 && coords.top < windowHeight
+
+    return topVisible
+}
+
+const showVisible = () => {
+    for (const el of els) {
+        if (isVisible(el)) {
+            el.classList.add('makeMeAppear')
+        }
+    }
+}
+
+// ver uma forma melhor de fazer isso.
+
+let title = document.querySelectorAll('[data-appear-title]')
+
+const showTitle = () => {
+    for (const ttl of title) {
+        if (isVisible(ttl)) {
+            ttl.classList.add('makeMeAppear')
+        }
+    }
+}
+
+const apply = () => {
+    showVisible()
+    showTitle()
+}
+
+window.addEventListener('scroll', apply)
+apply()
